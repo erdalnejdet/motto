569a6611acfb4993b8d4fb309ee3c22ba2247719
erdalnejdet <erdalnejdet1@gmail.com>
2025-11-29 16:42:27 +0300

    s

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ const runSequence = require('run-sequence');
 const clone = require('gulp-clone');
 const sass = require('gulp-sass')(require('sass'));
 
-
+// CSS
 function css() {
     return src('src/css/**/*.scss')
         .pipe(sass().on('error', sass.logError))
@@ -36,39 +36,44 @@ function css() {
         .pipe(browsersync.stream());
 }
 
+// JS
 function js() {
-    return src('src/js/**/*js')
+    return src('src/js/**/*.js')
         .pipe(minifyJS())
         .pipe(concat('app.min.js'))
         .pipe(dest('dist/js'))
         .pipe(browsersync.stream());
 }
 
+// HTML
 function html() {
     return src('src/**/*.html')
         .pipe(dest('dist'))
         .pipe(browsersync.stream());
 }
 
+// IMG
 function img() {
     return src('src/img/*')
-        .pipe(dest('dist/img'))
+        .pipe(dest('dist/img'));
 }
 
+// FONTS
 function font() {
     return src('src/fonts/*')
-        .pipe(dest('dist/fonts'))
+        .pipe(dest('dist/fonts'));
 }
 
-
+// WATCH
 function watchFiles() {
     watch('src/**/*.html', html);
     watch('src/css/**/*.scss', css);
     watch('src/js/**/*.js', js);
-    watch('src/img**/*', img);
-    watch('src/fonts**/*', font);
+    watch('src/img/**/*', img);
+    watch('src/fonts/**/*', font);
 }
 
+// LIVE SERVER
 function browserSync() {
     browsersync.init({
         server: {
@@ -78,6 +83,10 @@ function browserSync() {
     });
 }
 
+// DEFAULT TASK (local development)
 const watched = parallel(watchFiles, browserSync);
-
 exports.default = parallel(series(html, font, js, css, img), watched);
+
+// 🔥 BUILD TASK (PRODUCTION)
+const build = series(html, font, js, css, img);
+exports.build = build;
